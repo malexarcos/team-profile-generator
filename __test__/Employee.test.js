c44705f1c3cd2eb5b4c617c38ea55f5126cbd942
malexarcos <110743192+malexarcos@users.noreply.github.com>
2022-11-12 12:19:15 -0600

    added code to employeetest/js

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,2 +1,21 @@
-const Employee = require('../Employee');
+const Employee = require('../lib/Employee');
 
+test('Can create a new employee object', () => {
+    const employee  = new Employee();
+    expect(typeof(employee)).toBe('object');
+});
+
+test('Can set name in employee object', () => {
+    const employee = new Employee("alex");
+    expect(employee.name).toBe("alex");
+});
+
+test('Can set employee_id in employee object', () => {
+    const employee = new Employee("alex", 13);
+    expect(employee.employee_id).toBe(13);
+});
+
+test('Can set email in employee object', () => {
+    const employee = new Employee("alex", 13, "alex@employer.com");
+    expect(employee.email).toBe("alex@employer.com");
+});
